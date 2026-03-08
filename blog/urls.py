a0debc372cc21d31eb1d5be21118f8bbe12a579e
Abdo Sabry <abdelrahmansabry53@gmail.com>
2026-03-08 21:58:10 +0200

    create post details page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import posts
+from .views import posts,post_details
 
 urlpatterns = [
-    path('posts/',posts)
+    path('posts/',posts),
+    path('posts/<int:pk>',post_details)
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from .models import Post
 def posts(req):
     posts = Post.objects.all()
     return render(req,'posts.html',{'posts':posts})
+
+def post_details(req,pk):
+    post = Post.objects.get(id=pk)
+    return render(req,'post_details.html',{'post':post})
