2aa6c6bc3134594713e8122f29a906f80597d4c4
Abdo Sabry <abdelrahmansabry53@gmail.com>
2026-04-02 19:21:45 +0200

    create accounts app and login page

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,logout
+
+def login_view(req):
+    if req.method == "POST":
+        form = AuthenticationForm(data = req.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(req,user)
+            return redirect("all_posts")
+    else:
+        form = AuthenticationForm()
+    
+    return render(req,'accounts/login.html',{"form":form})
